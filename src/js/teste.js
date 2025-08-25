f8722e03d8704ea651dae48ce6c34c0b52fa3cb6
JOAO PEDRO CAVANHAS COLOMBO <joao.pccolombo@senacsp.edu.br>
2025-08-25 20:19:33 -0300

    feat: atividade html

---
+++ src/js/teste.js
@@ -0,0 +1,10 @@
+var x = 10; 
+let y = 20;
+
+if (true) {
+    var x = 30;
+    let y = 40;
+    console.log("dentro do if", x, y);
+  } 
+
+  console.log("fora do if", x, y);
---
+++ vetor.js
@@ -0,0 +1,13 @@
+// const notas = [5.5, 6.7, 10];
+
+// function Gmedia (notas) {
+//     let media = 0;
+//     for (nta of notas) {
+//         media += nta;
+//     }
+//     return media / notas.length;
+// }
+// let medi = Gmedia(notas); // chama a função
+// console.log(medi.toFixed(2)); // imprimir em duas casas decimais
+
+
